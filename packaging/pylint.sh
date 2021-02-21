20d4c741c3167da985531989a8978f0a95d97a33
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2021-02-21 14:30:43 +0000

    physcon module fully documented

--- packaging/pylint.sh
+++ packaging/pylint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd $(dirname ${0})/..
 # pylint h5utils/h5tovts.py
-pylint setup.py h5utils/
+# pylint setup.py h5utils/
+# pylint --disable=all --enable=trailing-whitespace h5utils/h5utils.py
+# pylint --disable=all --enable=trailing-whitespace setup.py h5utils/
+pylint --disable=all --enable=missing-function-docstring --enable=missing-module-docstring setup.py h5utils/
