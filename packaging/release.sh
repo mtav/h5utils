9616d8af1cf3631f60684d8eb2a6879587891116
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2021-02-21 14:52:46 +0000

    -bumpversion config file added -version bumping made optional in release.sh

--- packaging/release.sh
+++ packaging/release.sh
@@ -10,7 +10,16 @@ NAME=$(python3 setup.py --name)
 CURRENT_VERSION=$(python3 setup.py --version)
 
 # bump version
-bumpversion --current-version ${CURRENT_VERSION} patch setup.py
+echo "bump version? (y:yes,n:no,q:quit)"
+read ans
+case $ans in
+  y|Y|yes) bumpversion --verbose patch;;
+  q) exit;;
+  *) echo "keeping version=${CURRENT_VERSION}";;
+esac
+
+# bumpversion --current-version ${CURRENT_VERSION} patch setup.py
+# bumpversion --verbose patch
 
 # remove previous versions to avoid issues
 rm -iv dist/*
