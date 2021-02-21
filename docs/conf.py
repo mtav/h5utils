a4cc33ab670a7994fb39044f4d5897701ed14cb6
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2021-02-21 14:46:25 +0000

    Bump version: 0.0.2 → 0.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Mike Taverne'
 author = 'Mike Taverne'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.0.3'
 
 
 # -- General configuration ---------------------------------------------------
